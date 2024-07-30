9cf82cb57d0195ddca36abfb215351251f6eef17
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-07-30 21:34:36 +0530

    converted backend project to typescript

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+const app = express();
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(`Message received from ${data.author}: ${data.message}`);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
