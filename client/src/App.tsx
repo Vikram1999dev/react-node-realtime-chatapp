3006c09b63aff175305121cb7ab04f962887be00
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-07-31 11:29:15 +0530

    added bootstrap,changed the title,little improvments in UX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,18 @@ import React, { useState } from "react";
 // establish a WebSocket connection to a Socket.IO server.
 import { io } from "socket.io-client";
 import Chats from "./chat";
-const socket = io("http://localhost:3001");
+const socket = io("http://localhost:3002");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
 
-  const joinRoom = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const joinRoom = (
+    e:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLInputElement>,
+  ) => {
     e.preventDefault();
     if (username !== "" && room !== "") {
       //-->01
@@ -25,18 +29,25 @@ function App() {
     <div className='App'>
       {!showChat ? (
         <div className='joinChatContainer'>
-          <h3>Join a Chat</h3>
+          <h3 className='w-100'>Join a Chat</h3>
           <input
             type='text'
             placeholder='John...'
+            className='w-100'
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type='text'
             placeholder='Room ID'
+            className='w-100'
             onChange={(e) => setRoom(e.target.value)}
+            onKeyDown={(e) => {
+              e.key === "Enter" && joinRoom(e);
+            }}
           />
-          <button onClick={joinRoom}>Join a Room</button>
+          <button className='w-100' onClick={joinRoom}>
+            Join a Room
+          </button>
         </div>
       ) : (
         <Chats socket={socket} username={username} room={room} />
--- client/src/chat.tsx
+++ client/src/chat.tsx
@@ -1,8 +1,4 @@
 import React, { useEffect, useState } from "react";
-// ScrollToBottom is a component from the react-scroll-to-bottom library in
-// React, which can be used to create a chat interface or any other UI element
-// that needs to automatically scroll to the bottom when new content is added.
-// import ScrollToBottom from "react-scroll-to-bottom";
 import { Socket } from "socket.io-client";
 import { DefaultEventsMap } from "@socket.io/component-emitter";
 
@@ -47,22 +43,9 @@ const Chats = ({ socket, username, room }: ChatProps) => {
     }
   };
 
-  //here we used useEffect cleanup
-  //which basically stops
-  // when you're building an app with React, you might create some
-  // things that need to be "put away" when you're done with them.
-  // For example, you might create a timer that updates the app every
-  // second. But if you don't stop the timer when you're done, it keeps
-  // running in the background and can slow down your app.
-
-  // The cleanup function is also called when the dependencies specified in the
-  // useEffect hook change. In this case, the only dependency is the socket
-  // variable. If the socket variable changes, the useEffect hook will be called
-  // again, and the previous event listener will be removed before a new one is
-  // added.
   useEffect(() => {
     const handleReceiveMessage = (data: MessageType) => {
-      //data we get here is from server or another person in
+      //data we get here is from another person in
       //in the chat room
       setMessageList((list) => [...list, data]);
     };
@@ -70,7 +53,6 @@ const Chats = ({ socket, username, room }: ChatProps) => {
     socket.on("receive_message", handleReceiveMessage);
 
     return () => {
-      console.log("cleanup");
       socket.off("receive_message", handleReceiveMessage);
     };
   }, [socket]);
@@ -85,17 +67,24 @@ const Chats = ({ socket, username, room }: ChatProps) => {
           {messageList.map((messageContent, index) => {
             return (
               <div
-                className='message'
+                className={`message d-flex flex-column ${
+                  username === messageContent.author
+                    ? "align-items-end"
+                    : "align-items-start"
+                }`}
                 id={username === messageContent.author ? "you" : "other"}
                 key={index}
               >
-                <div>
-                  <div className='message-content'>
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className='message-meta'>
-                    <p id='time'>{messageContent.time}</p>
-                  </div>
+                <div className='person'>
+                  {username === messageContent.author
+                    ? "Me"
+                    : messageContent.author}
+                </div>
+                <div className='message-content'>
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className='message-meta'>
+                  <p id='time'>{messageContent.time}</p>
                 </div>
               </div>
             );
